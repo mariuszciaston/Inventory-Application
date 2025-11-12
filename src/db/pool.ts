c2d96cafbab356d56549cf01e623227467170174
Mariusz <casmar90@gmail.com>
2025-11-12 21:12:44 +0100

    update database connection configuration to use DATABASE_URL

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -3,8 +3,7 @@ import { Pool, QueryResultRow } from "pg";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_LOCAL_URL ?? process.env.DATABASE_PUBLIC_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 const query = async <T extends QueryResultRow>(
