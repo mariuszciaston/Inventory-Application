84759935e956603e2ad6d2b30c385483cb16e7f6
Mariusz <casmar90@gmail.com>
2025-12-14 16:38:47 +0100

    move platform views to separate folder

--- src/controllers/platformsController.ts
+++ src/controllers/platformsController.ts
@@ -10,17 +10,21 @@ import {
 
 async function renderPlatformsPage(_req: Request, res: Response) {
   const platforms = await getAllPlatforms();
-  res.render("platforms", { platforms });
+  res.render("platforms/platforms", { platforms });
 }
 
 async function renderGamesByPlatform(req: Request, res: Response) {
   const games = await getGamesByPlatform(req.params.id);
   const platform = await getPlatformById(req.params.id);
-  res.render("platform", { games, platformName: platform.name, platform });
+  res.render("platforms/platform", {
+    games,
+    platformName: platform.name,
+    platform,
+  });
 }
 
 async function renderNewPlatformForm(_req: Request, res: Response) {
-  res.render("newPlatformForm");
+  res.render("platforms/newPlatformForm");
 }
 
 async function submitNewPlatform(req: Request, res: Response) {
@@ -30,7 +34,7 @@ async function submitNewPlatform(req: Request, res: Response) {
 
 async function renderEditPlatformForm(req: Request, res: Response) {
   const platform = await getPlatformById(req.params.id);
-  res.render("platformEditForm", { platform });
+  res.render("platforms/platformEditForm", { platform });
 }
 
 async function changePlatformName(req: Request, res: Response) {
